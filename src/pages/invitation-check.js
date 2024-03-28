741f7918fd6282eefd5bbe3ffcb9408778b01239
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2024-03-28 23:56:07 +0300

    hope it works

--- src/pages/invitation-check.js
+++ src/pages/invitation-check.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import { useParams } from 'react-router-dom';
 
 const CheckInvitation = () => {
 	const [isRegistered, setIsRegistered] = useState(false);
